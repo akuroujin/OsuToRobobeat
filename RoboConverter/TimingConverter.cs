6e845c77b425e2e9e87a4020048a36010978df57
akuroujin <akuroujin@gmail.com>
2024-06-06 12:46:57 +0200

    removed delay on bpm conversion

--- RoboConverter/TimingConverter.cs
+++ RoboConverter/TimingConverter.cs
@@ -10,12 +10,12 @@ static class TimingConverter{
         return beats;
     }
     public static List<double> CovnertTiming(OsuMap map){
-        const double delay = 0.073; //Weird delay that gets added after importing
+        // const double delay = 0.073; //Weird delay that gets added after importing
         double startOffset;
         for (int i = 0; i < map.GetTimingPoints().Count; i++)
         {
             
-            startOffset = map.GetTimingPoints()[i].offset/1000.0 + delay;
+            startOffset = map.GetTimingPoints()[i].offset/1000.0;
             if(i+1 < map.GetTimingPoints().Count){
                 FillBeats(
                     startOffset, map.GetTimingPoints()[i+1].offset/1000.0, map.GetTimingPoints()[i].bpm/1000.0
@@ -27,7 +27,7 @@ static class TimingConverter{
                     );
             }
         }
-        beats.Add((map.lastObjectTime/1000.0) + delay);
+        beats.Add(map.lastObjectTime/1000.0);
         return beats;
     }
     private static void FillBeats(double startOffset, double endOffset, double bpm){
